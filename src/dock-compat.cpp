026b8378ebc3b5d21536bb12d079afb4bced0ca0
Norihiro Kamae <norihiro@nagater.net>
2023-07-08 00:28:29 +0900

    Add a class OBSDock
    To apply the style for `OBSDock QFrame`, define the class `OBSDock`,
    which just inherits from QDockWidget.

--- src/dock-compat.cpp
+++ src/dock-compat.cpp
@@ -1,19 +1,20 @@
 #include <obs-module.h>
 #include <obs-frontend-api.h>
 #include <QWidget>
-#include <QMainWindow>
-#include <QDockWidget>
+#include "dock-compat.hpp"
 
 #include "plugin-macros.generated.h"
 
 #define QT_UTF8(str) QString::fromUtf8(str, -1)
 
+OBSDock::OBSDock(QMainWindow *main) : QDockWidget(main) {}
+
 #if LIBOBS_API_VER < MAKE_SEMANTIC_VERSION(29, 1, 3)
 extern "C"
 bool obs_frontend_add_dock_by_id_compat(const char *id, const char *title, void *widget)
 {
-	auto *main = (QMainWindow*)obs_frontend_get_main_window();
-	QDockWidget *dock = new QDockWidget(main);
+	auto *main = (QMainWindow *)obs_frontend_get_main_window();
+	auto *dock = new OBSDock(main);
 
 	dock->setWidget((QWidget *)widget);
 	dock->setWindowTitle(QT_UTF8(title));
---
+++ src/dock-compat.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QDockWidget>
+
+// To apply the style for `OBSDock QFrame`, define the class `OBSDock`.
+class OBSDock : public QDockWidget
+{
+	Q_OBJECT
+public:
+	OBSDock(QMainWindow *main);
+};
