eb494e7918720d570e985891f7f8e9ee8cd70074
Norihiro Kamae <norihiro@nagater.net>
2025-10-11 01:51:22 +0900

    example: Add name parameter to get, pause, reset, and resume

--- example/get_loudness.py
+++ example/get_loudness.py
@@ -11,6 +11,7 @@ def _get_args():
     parser.add_argument('-r', '--reset', action='store_true')
     parser.add_argument('-p', '--pause', action='store_true')
     parser.add_argument('-s', '--resume', action='store_true')
+    parser.add_argument('--name', action='store', default=None)
     return parser.parse_args()
 
 def _main():
@@ -18,31 +19,36 @@ def _main():
 
     cl = obsws_python.ReqClient(host='localhost', port=4455)
 
+    data = {}
+    if args.name:
+        data['name'] = args.name
+
     if args.pause:
         cl.send('CallVendorRequest', {
             'vendorName': 'obs-loudness-dock',
             'requestType': 'pause',
-            'requestData': {'pause': True},
+            'requestData': data | {'pause': True},
         })
 
     if args.reset:
         cl.send('CallVendorRequest', {
             'vendorName': 'obs-loudness-dock',
             'requestType': 'reset',
+            'requestData': data,
         })
 
     if args.resume:
         cl.send('CallVendorRequest', {
             'vendorName': 'obs-loudness-dock',
             'requestType': 'pause',
-            'requestData': {'pause': False},
+            'requestData': data | {'pause': False},
         })
 
     if not (args.pause or args.resume or args.reset):
         res = cl.send('CallVendorRequest', {
             'vendorName': 'obs-loudness-dock',
             'requestType': 'get_loudness',
-            'requestData': {},
+            'requestData': data,
         })
         for field in ('momentary', 'short', 'integrated', 'range', 'peak'):
             value = res.response_data[field]
